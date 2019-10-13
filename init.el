25262e71850c634bd2da9bc2b313d89319fbbb36
promesante <lmrosso@gmail.com>
2019-10-12 21:07:11 -0300

    settings for elm development

--- init.el
+++ init.el
@@ -3,23 +3,4 @@
 ;; packages in elisp dir
 (load-library "melpa")
 (load-library "clojure")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (flycheck-elm flycheck elm-mode rainbow-delimiters monokai-theme highlight-symbol helm-ag company cider)))
- '(safe-local-variable-values
-   (quote
-    ((cider-ns-refresh-after-fn . "integrant.repl/resume")
-     (cider-ns-refresh-before-fn . "integrant.repl/suspend")
-     (cider-refresh-after-fn . "integrant.repl/resume")
-     (cider-refresh-before-fn . "integrant.repl/suspend")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load-library "elm")
